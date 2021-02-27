c8e5acee8a0577422332dac8c2f50e45850d72ee
Benjamin Macias <jacobmac10@hotmail.com>
2021-02-26 20:37:48 -0700

    Se entrego un conjunto vacio al usuario, posteriormente dimos la opcion de "pedir carta" o "No pedir carta"

--- Program.cs
+++ Program.cs
@@ -4,9 +4,44 @@ namespace Blackjack
 {
     class Program
     {
+      
+        /// <summary>
+        /// Entregar a usuario conjunto de cartas vacio, 
+        ///  pedir a usuario que nos pida una carta
+        /// </summary>
+        /// <returns>selecciona 1 - pedir carta</returns>
+
+        static int SelectOption()
+        {
+            //pedir al usuario que nos pida una carta para comenzar juego
+            // 1: pedir carta
+            
+            Console.WriteLine("Conjunto de cartas vacio, ¿Quieres una carta?");
+            Console.WriteLine("1) Pedir carta");
+            Console.WriteLine("2) No");
+            
+
+            int opcionCarta = Convert.ToInt32(Console.ReadLine());
+            return opcionCarta;
+        }
+        
+
+        
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcionCarta = SelectOption();
+            //Console.WriteLine("mano: " + opcionCarta);
+
+         
+         
+            
+
+            
+            
+
         }
     }
 }
+        
