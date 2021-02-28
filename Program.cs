56cd5dce2868b891e86f61e31c55c6aeeada284e
Benjamin Macias <jacobmac10@hotmail.com>
2021-02-28 12:10:38 -0700

    Se actualizo el codgo, separando la funcion de cartas vacias y pedir nueva carta

--- Program.cs
+++ Program.cs
@@ -7,22 +7,36 @@ namespace Blackjack
       
         /// <summary>
         /// Entregar a usuario conjunto de cartas vacio, 
-        ///  pedir a usuario que nos pida una carta
+        ///  
         /// </summary>
-        /// <returns>selecciona 1 - pedir carta</returns>
+        /// <returns>el usuario tiene 0 cartas</returns>
 
-        static int SelectOption()
+        static int conjuntoDeCartas()
         {
-            //pedir al usuario que nos pida una carta para comenzar juego
-            // 1: pedir carta
-            
-            Console.WriteLine("Conjunto de cartas vacio, ¿Quieres una carta?");
-            Console.WriteLine("1) Pedir carta");
-            Console.WriteLine("2) No");
-            
+           // entregamos al jugador un conjunto de cartas vacio
+            int cartasjugador = 0;
 
+            Console.WriteLine("Empecemos, su mano tiene " + cartasjugador + " cartas");
+           
+            
+            
             int opcionCarta = Convert.ToInt32(Console.ReadLine());
             return opcionCarta;
+            
+
+        }
+        /// <summary>
+        /// preguntar si el jugador desea una nueva carta
+        /// </summary>
+        /// <returns>1) pedira carta, 2) No quiero</returns>
+        static int pedirNuevaCarta()
+        {
+            Console.WriteLine("Conjunto de cartas vacio, ¿Quieres una carta?");
+            Console.WriteLine("1) Pedir carta");
+            Console.WriteLine("2) No quiero");
+
+            int nuevaCarta = Convert.ToInt32(Console.ReadLine());
+            return nuevaCarta;
         }
         
          static int MostrarCartaRandom()
@@ -30,10 +44,10 @@ namespace Blackjack
             /// <summary>
             ///  Si el jugador solicita una carta, generar aleatoriamente una carta de una baraja y agregarla a las cartas que tiene el jugador.
             /// </summary>
-            /// <returns>La carta random que genre el sistema del 1 al 11</returns>
+            /// <returns>La carta random que genre el sistema del 1 al 12</returns>
             Random rnd = new Random();
-            //numeros random del 1 al 11
-            int option = rnd.Next(1, 11);
+            //numeros random del 1 al 11 tomamos as como 11
+            int option = rnd.Next(1, 12);
             return option;
         }
 
@@ -42,11 +56,14 @@ namespace Blackjack
 
         static void Main(string[] args)
         {
-            int opcionCarta = SelectOption();
-            //Console.WriteLine("mano: " + opcionCarta);
+            int opcionCarta = conjuntoDeCartas();
+            
+            int nuevaCarta = pedirNuevaCarta();
+
              //TODO: Se entrega la carta random
             int aiOption = MostrarCartaRandom();
             Console.WriteLine("Mano: " + aiOption);
+         
 
          
          
