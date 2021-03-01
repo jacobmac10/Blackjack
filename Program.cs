099ad6e306a369853899d9ad13ae7b8321b9d7fd
Benjamin Macias <jacobmac10@hotmail.com>
2021-02-28 21:02:06 -0700

    funcionalidad de mostrar los numeros obtenidospor el jugador y por la casa

--- Program.cs
+++ Program.cs
@@ -36,37 +36,37 @@ namespace Blackjack
         /// </summary>
         /// <returns>S para si, N para no</returns>
 
-        static string PedirCartas()
-        {
+        // static string PedirCartas()
+        //{
 
-            Console.WriteLine("¿Desea una nueva carta?");
-            Console.WriteLine("s = si"); // s para si 
-            Console.WriteLine("n = no"); //n para no 
+        // Console.WriteLine("¿Desea una nueva carta?");
+        //Console.WriteLine("s = si"); // s para si 
+        //Console.WriteLine("n = no"); //n para no 
 
-            string opcion = Convert.ToString(Console.ReadLine());
-            return opcion;
-        }
+        //string opcion = Convert.ToString(Console.ReadLine());
+        //return opcion;
+        // }
         /// <summary>
         /// haremos una condicional if, para ver si le damos una carta al jugador dependiendo de la opcion que eligio
         /// </summary>
         /// <param name="CartasJugador">conjunto con el que comenzó (0)</param>
         /// <param name="opcion">eleccion del jugador sobre si desea una carta nueva</param>
         /// <param name="cartaAleatoria">carta generada de manera aleatoria del 1 al 11</param>
-        static void EntregaDeCarta(int CartasJugador, string opcion, int cartaAleatoria)
-        {
-            if (opcion == "s")
-            {
-                Console.WriteLine("cartas en mano: " + cartaAleatoria);
-            }
-            else if (opcion == "n")
-            {
-                Console.WriteLine("cartas en mano: " + CartasJugador);
-            }
-            else
-            {
-                Console.WriteLine("comando no valido, por favor, elija una de las siguientes opciones: S= si, N = no");
-            }
-        }
+        //static void EntregaDeCarta(int CartasJugador, string opcion, int cartaAleatoria)
+        //{
+        //   if (opcion == "s")
+        // {
+        //     Console.WriteLine("cartas en mano: " + cartaAleatoria);
+        //}
+        //else if (opcion == "n")
+        //{
+        //Console.WriteLine("cartas en mano: " + CartasJugador);
+        //}
+        //else
+        //{
+        //  Console.WriteLine("comando no valido, por favor, elija una de las siguientes opciones: S= si, N = no");
+        //}
+        //}
         /// <summary>
         /// sumatoria de puntos del jugador 
         /// </summary>
@@ -99,11 +99,11 @@ namespace Blackjack
             while (suma < 21)
             {
                 Console.WriteLine("¿Desea una nueva carta?");
-                Console.WriteLine("s = si"); // s para si 
-                Console.WriteLine("n = no"); //n para no 
-                string opcion = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("1) = si"); // s para si 
+                Console.WriteLine("2) = no"); //n para no 
+                int opcion = Convert.ToInt32(Console.ReadLine());
 
-                if (opcion == "s")
+                if (opcion == 1)
                 {
                     Random rnd = new Random();
                     int aiCarta = rnd.Next(1, 12);
@@ -118,17 +118,42 @@ namespace Blackjack
                   ///Cuando el jugador se detenga, generar aleatoriamente un número entre el 17 y el 26 (incluyendo ambos números). Este será el valor de "la casa" contra el cual debe competir el número obtenido por las cartas del jugador.
 
                   ///
-                else if (opcion == "n")
+                else if (opcion == 2)
                 {
 
                     Random rnd = new Random();
                     int lehouseAi = rnd.Next(17, 26);
                     Console.WriteLine("La casa tiene: " + lehouseAi);
 
+                    // El jugador obtiene un 21 y la casa obtiene un número menor
+                    if (suma == 21 && lehouseAi != 21)
+                    {
+                        Console.WriteLine("Gano " + suma);
+                    }
+                    else if (suma > lehouseAi && suma < 21)
+                    {
+                        Console.WriteLine("Gano " + suma);
+                    }
+                    //La casa obtiene un número mayor a 21 y el jugador obtiene un número menor o igual que 21.
+                    if (lehouseAi > 21 && suma <= 21)
+                    {
+                        Console.WriteLine("Gano " + suma);
+                    }
+                    //El jugador y la casa obtienen un número menor o igual a 21, pero el jugador obtiene un número más alto.
+                    else if (suma > lehouseAi && suma < 21)
+                    {
+                        Console.WriteLine("Gano  " + suma);
+                    }
+                    //La casa obtiene un número mayor a 21 y el jugador obtiene un número menor o igual que 21.
+                    if (lehouseAi > 21 && suma <= 21)
+                    {
+                        Console.WriteLine("Ganó el jugador con " + suma);
+                    }
+                    Console.WriteLine(CartasJugador);
 
+                    break;
                 }
             }
-
         }
 
 
